8e1f75be538df5f0330a3908878fc0a88cdf4015
Shadaf Hossain <shadafhossain01@gmail.com>
2026-05-02 23:44:29 +0600

    User Model Design Done

---
+++ src/model/user.model.js
@@ -0,0 +1,54 @@
+const mongoose=require("mongoose")
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name must be required for Creating Bank Account"],
+    minLength: [2, "Name must be contain over 2 characters"],
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique:true,
+    lowercase: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email must be required for Creating Bank Account",
+    ],
+  },
+  password: {
+    type: String,
+    select: false,
+    minLength: [5, "Password must be contain over 5 digits"],
+    required: [true, "Password must be required for Creating Bank Account"],
+  },
+  
+});
+
+userSchema.pre("save", async function(){
+    if(!this.isModified(password)) return
+
+    this.password =await bcrypt.hash(this.password,10)
+} )
+
+userSchema.methods.comparePassord=function(password){
+    return bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateToken=function(){
+    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    return token
+}
+
+userSchema.methods.verifyToken=async function(token){
+    const decodedData = await jwt.verify(token, process.env.JWT_SECRET);
+    return decodedData;
+}
+
+const User=mongoose.model("User",userSchema)
+module.exports=User

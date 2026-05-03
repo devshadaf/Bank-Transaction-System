0b8d65cfa6a6cf3a686ab481b0c55eb4c3ef0ebd
Shadaf Hossain <shadafhossain01@gmail.com>
2026-05-04 03:14:37 +0600

    Update transition.model.js

--- src/model/transition.model.js
+++ src/model/transition.model.js
@@ -1,25 +1,32 @@
 const mongoose= require("mongoose")
 
-const transitionSchema = new mongoose.Schema({
-  fromUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const transitionSchema = new mongoose.Schema(
+  {
+    fromUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    toUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ammount: {
+      type: Number,
+      required: true,
+    },
+    transitionType: {
+      type: String,
+      enum: ["Credit", "Debit"],
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Failed", "Success"],
+    },
   },
-  toUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  ammount:{
-    type:Number,
-    required:true
-  },
-  transitionType:{
-    type:String,
-    enum:["Credit","Debit"]
-  }
-},{timestamps:true});
+  { timestamps: true },
+);
 
 const Transition = mongoose.model("Transition", transitionSchema);
 module.exports = Transition;
